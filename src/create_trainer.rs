f53dc9ad8e6c218d3ee796ff49d51f443bac4a9f
Ryan Knuesel <ryan.knuesel@gmail.com>
2021-10-30 14:52:57 -0500

    Fixes logic error and prevents overwrite on createTrainer

--- src/create_trainer.rs
+++ src/create_trainer.rs
@@ -9,23 +9,24 @@ pub fn create_trainer<'a>(
     username: &str,
     password: &str,
 ) -> Result<Trainer, &'a str> {
+    let result = sled.trainers.get(username);
+    if let Ok(Some(_)) = result {
+        return Err("Trainer already exists");
+    }
+
     let new_trainer = TrainerInternal {
         name: username.into(),
         password: password.into(),
         cash: 100,
         pet_ids: vec![],
     };
-    // RK: REPORT: I noticed that neither Ok(Some()) nor Err() match result. This probably means it's Ok(None),
-    //             which probably means that I can just return new_trainer.
-    let result = sled.trainers.insert(username, new_trainer);
-    if let Ok(Some(new_trainer)) = result {
+
+    let result = sled.trainers.insert(username, new_trainer.clone());
+    if let Ok(None) = result {
         return Ok(Trainer {
             name: new_trainer.name,
             cash: new_trainer.cash,
         });
-    }
-    if let Err(msg) = result {
-        print!("{:?}", msg);
     }
 
     Err("Could not save trainer")
